e1325df81f7a4dec2e275f0edb8e4e2503aa75d4
Kelii DeLeon <keliikeideleon@gmail.com>
2023-09-02 11:35:59 -0600

    Working Madlibs

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "bananas"
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def get_prompts():
+    prompts = story.prompts
+
+    return render_template('index.html', prompts=prompts)
+
+@app.route('/story')
+def show_story():
+    text = story.generate(request.args)
+
+    return render_template('story.html', text=text)
